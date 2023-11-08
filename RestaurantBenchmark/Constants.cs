7814effd6716ff1cb359dfe4cc591397fa814f03
admir-live <admir.mujkic@infinitymesh.com>
2023-11-09 00:17:28 +0100

    Update database connection string in Constants
    The existing hard-coded database connection string has been replaced with a placeholder ("<connection string>"). This encourages users to provide their own credentials and reinforces the security of the database connection.

--- RestaurantBenchmark/Constants.cs
+++ RestaurantBenchmark/Constants.cs
@@ -3,7 +3,7 @@
 public static class Constants
 {
     public const string ConnectionString =
-        @"Server=.,1434;Database=RestaurantDB;User Id=sa;Password=1Strong#Pwd;MultipleActiveResultSets=True;TrustServerCertificate=True;Encrypt=false";
+        @"<connection string>";
 
     public const int MainDishesCount = 10000;
     public const int SideDishesCount = 20000;
